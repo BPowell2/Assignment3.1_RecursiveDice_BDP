c8422c8b208592e85d9ccfbb1688147a632ce481
BPowell2 <112312288+BPowell2@users.noreply.github.com>
2022-10-18 04:38:18 -0400

    Create RecDieRollerDriver.java

---
+++ RecDieRollerDriver.java
@@ -0,0 +1,12 @@
+/* This is the main method using the RecDieRoller class
+ * 10/18/22
+ * @author Benjamin Powell
+ * @version 1.0
+ * @since 2022-18-10
+ */
+public class RecDieRollerDriver {
+
+	public static void main(String []args) {
+		RecDieRoller.diceRoll(2, 100,  0, 0);
+	}
+}//end of class
